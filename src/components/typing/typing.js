e95352ed272dcf4ac5ee52c3833ca1e44c0cacd6
Unknown <teethtard321@gmail.com>
2019-10-04 15:36:35 +0000

    Added marking input text with red color when typed word is incorrect effect

--- src/components/typing/typing.js
+++ src/components/typing/typing.js
@@ -18,19 +18,41 @@ export default {
 
       /* Settings */
       typing_time: 30,
-      number_of_words: 10,
+      number_of_words: 50,
+      is_typing_started: false,
+      typed_seconds: 0,
 
       /* Current word index */
       current_word_index: 0,
 
       /* Current user input-text value */
       current_user_text: "",
+
     }
   },
   computed: {
 
     current_word: function(){
       return this.typing_words[this.current_word_index];
+    },
+
+    /* This method creates styles for input */
+    input_style: function(){
+
+      return styles
+    },
+
+    /* Boolean that represents if user input is correct */
+    is_current_input_correct: function(){
+
+      let cut_current_word = this.current_word.substring(0, this.current_user_text.length);
+
+      if(cut_current_word == this.current_user_text){
+        return true;
+      }else{
+        return false;
+      }
+
     }
 
   },
@@ -47,6 +69,13 @@ export default {
       }
     }
     this.typing_words_left = this.typing_words;
+
+    /* Starting timer */
+    this.$nextTick(function () {
+    window.setInterval(() => {
+        this.CountDown();
+      },1000);
+    })
   },
   methods: {
 
@@ -55,15 +84,32 @@ export default {
       this.typing_words_left.pop();
       this.current_user_text = null;
       this.current_word_index++;
+      this.is_current_input_correct = false;
     },
 
     /* input-text onchange handler */
     TextOnchange: function(value){
       /* If user pressed space, lets go for next word */
-      console.log("textonchage")
       if(value == " "){
         this.next_word();
+      }else{
+        console.log(value)
       }
+    },
+
+    /* If typing is started every second change timer number, and stop typing when time is gone */
+    CountDown: function(){
+      if(this.is_typing_started){
+        this.typed_seconds++;
+        if(typed_seconds > this.typing_time){
+          this.finish();
+        }
+      }
+    },
+
+    /* Function called when typing finished (when time is gone, or all of the text was typed) */
+    finish: function(){
+      console.log("FINISHING")
     }
 
   },
